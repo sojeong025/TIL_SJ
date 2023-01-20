7ce9482e73e074483e69c9aad64c5bdc1b9b031a
정소정 <thwjd0205@naver.com>
2023-01-20 11:33:13 +0900

    moving

---
+++ python/recur.py
@@ -0,0 +1,16 @@
+def factorial(n):
+    if n==0 or n==1: ...
+    else:
+        return n * factorial (n-1)
+print(factorial(2))
+
+
+def sum(n):
+    if n==5:
+        return
+    else:
+        return (n+1)
+print(sum(5))
+
+
+
