d3605c612c5c217b7e8cb6e1d04c5602a889cf07
sojeong025 <thwjd025@gmail.com>
2023-03-24 01:44:40 +0900

    finish 1project

--- 05.django/Tstory/blog1/first_pjt/urls.py
+++ 05.django/Tstory/blog1/first_pjt/urls.py
@@ -1,4 +1,4 @@
-"""crud URL Configuration
+"""first_pjt URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -14,13 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+from articles import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('articles/', include('articles.urls')),
-    path('accounts/', include('accounts.urls')),
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    path('articles/', views.index),
+]
--- 05.django/Tstory/blog2/ORM/urls.py
+++ 05.django/Tstory/blog2/ORM/urls.py
@@ -1,4 +1,4 @@
-"""crud URL Configuration
+"""ORM URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -16,11 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from django.conf import settings
-from django.conf.urls.static import static
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('articles/', include('articles.urls')),
-    path('accounts/', include('accounts.urls')),
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+]
