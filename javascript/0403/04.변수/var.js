ef2f0c092072ed73f0e1389ba1f6977874b8f5dd
정소정 <thwjd0205@naver.com>
2023-04-03 17:46:24 +0900

    start javascript

---
+++ javascript/0403/04.변수/var.js
@@ -0,0 +1,11 @@
+// 변수는 하나의 값을 저장하기 위한 수단
+var userId = 1;
+var userName = "Jeong";
+
+// 객체나 배열 같은 자료구조 사용하면 여러 개의 값을 하나로 그룹화해서 하나의 값처럼 사용 가능
+var user = { id: 1, name: "Jeong" };
+
+var users = [
+  { id: 1, name: "Jeong" },
+  { id: 2, name: "Kim" },
+];
